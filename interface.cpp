3700c05a3b76380b8c35b4eef42cc7b5bf2a640f
Eric P <cire.ttalp@gmail.com>
2020-04-28 09:19:27 -0700

    Update interface

--- interface.cpp
+++ interface.cpp
@@ -3,14 +3,13 @@
 #include <iostream>
 
 void printBoard(const Board board) {
+	std::cout << "  1 2 3 4 5 6 7 8     Computer vs. Opponent\n";
 	for (int i = 0; i < 8; ++i) {
+		std::cout << char('A' + i);
 		for (int j = 0; j < 8; ++j) {
-			if (board.board[i*8+j]) {
-				std::cout << 'X' << ' ';
-			} else {
-				std::cout << (i*8+j) << ' ';
-			}
+			std::cout << ' ' << board.board[i*8+j];
 		}
-		std::cout << "\n";
+		std::cout << std::endl;
+
 	}
 }
