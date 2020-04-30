c8a2d95f12b050b2b1526bc89113a85aadc3c619
Eric P <cire.ttalp@gmail.com>
2020-04-30 15:00:15 -0700

    Agent vs self

--- main.cpp
+++ main.cpp
@@ -3,26 +3,50 @@
 #include "board.h"
 #include "interface.h"
 
-
 int main() {
 	std::vector<int> log;
 	Board board{};
 
+	/*
 	if (computerStartGame()) {
 		int value = getAiTurn(board);
 		std::cout << value << '\n';
 		board.moveComputer(value);
 		printBoard(board, log);
 	}
+	*/
 
 	while (1) {
-		int move = getUserInput(board);
-		board.moveOpponent(move);
+	
 		int value = getAiTurn(board);
 		board.moveComputer(value);
+
 		printBoard(board, log);
+	
+
+		if (board.gameOver()) {
+			break;
+		}
+
+		board.swapPlayer();
+
+
+		value = getAiTurn(board);
+		board.moveComputer(value);
+
+		board.swapPlayer();
+		printBoard(board, log);
+	
+
+		if (board.gameOver()) {
+			break;
+		}
+
 	}
-	getAiTurn(board);
+
+	std::cout << "\nFinal Board\n";
+	
+	printBoard(board, log);
+	
 	return 0;
 }
-
